d151e6200b9003007d99e41f340f131829a32f1d
Christine Zimmerman <ChristineOrtega@gmail.com>
2017-11-08 14:52:20 -0800

    Contain only the node server

--- serve.js
+++ serve.js
@@ -1,5 +1,13 @@
 const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const { schema, root } = require('./buildSchema.js');
+
 const app = express();
+app.use('/', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true
+}));
 
 app.listen(4000);
 console.log('Listening on 4000 ...');
