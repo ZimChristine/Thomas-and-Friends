82da0ee23fe9f69a19acd4b93573eff8fc1cc383
Christine Zimmerman <ChristineOrtega@gmail.com>
2017-11-08 14:53:25 -0800

    Build a JSON file for Thomas and Percy

---
+++ data/engines.js
@@ -0,0 +1,16 @@
+const Engines = [
+  {
+    id: 1,
+    name: 'Thomas',
+    color: 'blue',
+    number: '1'
+  },
+  {
+    id: 2,
+    name: 'Percy',
+    color: 'green',
+    number: '6'
+  }
+];
+
+module.exports = Engines;
